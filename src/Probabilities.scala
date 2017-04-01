3d675b777e5655592df0d6a4718e921d6b24007a
headinthebox <emeijer@applied-duality.com>
2017-04-01 15:06:47 -0700

    Simple Metropolis example

--- src/Probabilities.scala
+++ src/Probabilities.scala
@@ -1,29 +1,34 @@
-import Probabilities._
-
 import scala.util.Random
 
-object main {
+import vegas._
+import vegas.render.WindowRenderer._
+
+object Probabilities {
 
   def main(args: Array[String]): Unit = {
 
-    val xs = uniform(0,1,1,1)
-    //println(xs.take(1000000).groupBy(n => n).map(xn => (xn._1, xn._2.size)))
+    val N = 1000000
 
+    val rr = Stream.continually{ Math.random() <= 1/2.0 }
+    println(rr.take(N).groupBy(n => n).map(xn => (xn._1, xn._2.size/N.toDouble)))
+
+    val xs = uniform(0,1)
+    println(xs.take(N).groupBy(n => n).map(xn => (xn._1, xn._2.size/N.toDouble)))
+
+    val ys = metropolis(xs)(n => if(n==0) 2 else 1)
+    println(ys.take(N).groupBy(n => n).map(xn => (xn._1, xn._2.size/N.toDouble)))
     val zs = uniform((0,1), (1,2), (1,1)).take(100000)
-    println(zs.groupBy(xp=>xp._1).map(xn => (xn._1, xn._2.map(xn=> xn._2).sum)))
+    //println(zs.groupBy(xp=>xp._1).map(xn => (xn._1, xn._2.map(xn=> xn._2).sum)))
 
     val ws = metropolis2(zs)(x => if(x==0) 3 else 1)
     println(ws.groupBy(xp=>xp._1).map(xn => (xn._1, xn._2.map(xn=> xn._2).sum)))
 
-    val ys = metropolis(xs)(n => 1)
+    //val ys = metropolis(xs)(n => 1)
     //println(ys.take(1000000).groupBy(n => n).map(xn => (xn._1, xn._2.size)))
     // incremental groupby with Dirichlet process/distribution until confidence close enough
     // (normalization)
     // (x,p) --> add p to alpa for x
   }
-}
-
-object Probabilities {
 
   def repeat[A](a: A): Iterable[A] = {
     new Iterable[A] {
@@ -34,6 +39,7 @@ object Probabilities {
         }
     }
   }
+
   def uniform[A](values: A*): Iterable[A] = {
     new Iterable[A] {
       override def iterator: Iterator[A] = new Iterator[A] {
@@ -43,6 +49,7 @@ object Probabilities {
       }
     }
   }
+
   def scanl[A](xs: Iterable[A])(combine: (A,A)=> A): Iterable[A] = {
     new Iterable[A] {
       override def iterator: Iterator[A] = {
@@ -72,11 +79,35 @@ object Probabilities {
   }
 
   def metropolis[A](prior: Iterable[A])(likelyhood: A=>Double): Iterable[A] = {
-    scanl(prior)((o, n) => if(Random.nextDouble() <= likelyhood(n) / likelyhood(o)) n else o) // <?
+    scanl(prior)((o, n) => if(Math.random() <= likelyhood(n) / likelyhood(o)) n else o) // <?
   }
 
   def metropolis2[A](prior: Iterable[(A, Int)])(likelyhood: A=>Int): Iterable[(A,Int)] = {
     prior.map(xn => (xn._1, likelyhood(xn._1)*xn._2))
   }
+
+  def fac(n: Int): Int = Range(1,n+1).product
+
+  def Beta(alpha: Int, beta: Int)(x: Double) =
+    Math.pow(x, alpha-1)* Math.pow(1-x, beta-1)/
+      (fac(alpha)*fac(beta)/fac(alpha+beta))
+
   // http://www.ics.uci.edu/~johnsong/papers/Chib%20and%20Greenberg%20-%20Understanding%20the%20Metropolis-Hastings%20Algorithm.pdf
 }
+
+/*
+val plot = Vegas("Country Pop").
+      withData(
+        Seq(
+          Map("country" -> "USA", "population" -> 314),
+          Map("country" -> "UK", "population" -> 64),
+          Map("country" -> "DK", "population" -> 80)
+        )
+      ).
+      encodeX("country", Nom).
+      encodeY("population", Quant).
+      mark(Bar)
+
+    plot.show
+
+ */
---
+++ src/Streaming.scala
@@ -0,0 +1,58 @@
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.chart.XYChart.{Data, Series}
+import javafx.scene.chart.{NumberAxis, ScatterChart, XYChart}
+import javafx.stage.Stage
+
+import scala.util.Random
+//http://koaning.io/bayesian-propto-streaming-algorithms.html
+
+object Streaming {
+
+  def main(args: Array[String]): Unit = {
+
+    val xs = Range(1,25)
+
+    println(xs.map(_ => Random.nextDouble()*2))
+
+    Application.launch(classOf[Plot])
+  }
+}
+
+// If x is a sample from a mean 0 and variance 1 distribution then
+// σx+μ is a sample with mean μ and variance σ^2.
+
+class Plot extends Application {
+
+  override def start(primaryStage: Stage): Unit = {
+
+    val series = new XYChart.Series[Number, Number]()
+
+    val xs = Range(1,25).map(_ => Random.nextDouble()*2)
+    val xys: Seq[Data[Number, Number]] = xs.map(x => new XYChart.Data[Number, Number](
+      x,
+      2.5 + 3.5*x+Random.nextGaussian()*0.3))
+
+    xys.forall(series.getData.add(_))
+
+    val sc = new ScatterChart(new NumberAxis(-0.5, 2.5, 0.5), new NumberAxis(0, 10, 1))
+
+    sc.getData.setAll(series)
+
+    val scene  = new Scene(sc, 300, 250)
+
+    primaryStage.setScene(scene)
+    primaryStage.show
+
+    def next(x: Double, y: Double)(slope: Double, intercept: Double) =
+      Random.nextGaussian()*0.3 + (intercept + slope*x - y)
+
+    var slope = Random.nextDouble()
+    var intercept = Random.nextDouble()
+
+    xys.foreach(xy => {
+
+    })
+
+  }
+}
